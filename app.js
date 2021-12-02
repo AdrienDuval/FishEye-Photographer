0337607772e6fbd2f9b41e80d46113d5a1c2955c
AdrienDuval <88292518+AdrienDuval@users.noreply.github.com>
2021-12-03 01:29:27 +0530

    working on like button on the photographer page

--- app.js
+++ app.js
@@ -4,8 +4,12 @@ const previews = document.querySelectorAll(".single-image img");
 const original = document.querySelector(".full-img");
 const caption = document.querySelector(".caption");
 const errorMsg = document.querySelectorAll(".error-msg");
+const likeBtn = document.querySelectorAll(".like-btn");
+const likeCount = document.querySelectorAll(".like-count");
 
 let isFormValide =  false;
+let dislike = true;
+
 // declaring a variable for the id to avoid to much code repetion
 
 let id = (id) => document.getElementById(id);
@@ -38,8 +42,12 @@ previews.forEach((preview) => {
         original.src = originalSrc;
         const altText = preview.alt;
         caption.textContent = altText;
+        
+        console.log("yo");
     });
 });
+
+// close button for imnage pop up
 
 function closeBtns () {
     modal.classList.remove("open");
@@ -47,6 +55,9 @@ function closeBtns () {
     navBtn.style.display = "none";
     closeBtn.style.display = "none";
 }
+
+// opening modal form 
+
 function openModalForm () {
     modalForm.classList.remove("display-none");
     form.classList.add("open");
@@ -117,6 +128,11 @@ form.addEventListener('submit', (e) => {
         form.remove();
         receptioMsg.classList.remove("display-none");
     }
+    // printing form value in the console log 
+
+    console.log('first name ' + firstName.value );
+    console.log('last name ' + lastName.value );
+    console.log('email ' + email.value);
 });
 lastName.addEventListener("input", () => {
     validatelastNameInput();
@@ -128,6 +144,168 @@ email.addEventListener("input", () => {
     validateemailInput();
 });
 
+
+// increment likes on like button on photographer single page
+
+var valueCount
+
+function incrementLikes(nbr) {
+        valueCount = likeCount[nbr].value;
+        valueCount++;
+        likeCount[nbr].value = valueCount;
+        dislike = false;
+
+}
+
+function decrementLikes(nbr) {
+        valueCount = likeCount[nbr].value;
+        valueCount--;
+        likeCount[nbr].value = valueCount;
+        dislike = true;
+}
+function addlikecolor(nbr) {
+    likeBtn[nbr].classList.add("like");
+}
+function removelikecolor(nbr) {
+    likeBtn[nbr].classList.remove("like");
+}
+
+
+
+likeBtn[0].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(0);
+        addlikecolor(0);
+    } else {
+        decrementLikes(0);
+        removelikecolor(0);
+    };
+});
+
+likeBtn[1].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(1);
+        addlikecolor(1);
+    } else {
+        decrementLikes(1);
+        removelikecolor(1);
+    };
+});
+likeBtn[2].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(2);
+        addlikecolor(2);
+    } else {
+        decrementLikes(2);
+        removelikecolor(2);
+    };
+});
+likeBtn[3].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(3);
+        addlikecolor(3);
+    } else {
+        decrementLikes(3);
+        removelikecolor(3);
+    };
+});
+likeBtn[4].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(4);
+        addlikecolor(4);
+    } else {
+        decrementLikes(4);
+        removelikecolor(4);
+    };
+});
+likeBtn[5].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(5);
+        addlikecolor(5);
+    } else {
+        decrementLikes(5);
+        removelikecolor(5);
+    };
+});
+likeBtn[6].addEventListener('click', () => {
+    
+    if (dislike) {
+        incrementLikes(6);
+        addlikecolor(6);
+    } else {
+        decrementLikes(6);
+        removelikecolor(6);
+    };
+});
+
+photographers = [
+    {
+      "name": "Mimi Keel",
+      "id": 243,
+      "city": "London",
+      "country": "UK",
+      "tags": ["portrait", "events", "travel", "animals"],
+      "tagline": "Voir le beau dans le quotidien",
+      "price": 400,
+      "portrait": "MimiKeel.jpg"
+    },
+    {
+      "name": "Ellie-Rose Wilkens",
+      "id": 930,
+      "city": "Paris",
+      "country": "France",
+      "tags": ["sports", "architecture"],
+      "tagline": "Capturer des compositions complexes",
+      "price": 250,
+      "portrait": "EllieRoseWilkens.jpg"
+    },
+    {
+      "name": "Tracy Galindo",
+      "id": 82,
+      "city": "Montreal",
+      "country": "Canada",
+      "tags": ["art", "fashion", "events"],
+      "tagline": "Photographe freelance",
+      "price": 500,
+      "portrait": "TracyGalindo.jpg"
+    },
+    {
+      "name": "Nabeel Bradford",
+      "id": 527,
+      "city": "Mexico City",
+      "country": "Mexico",
+      "tags": ["travel", "portrait"],
+      "tagline": "Toujours aller de l'avant",
+      "price": 350,
+      "portrait": "NabeelBradford.jpg"
+    },
+    {
+      "name": "Rhode Dubois",
+      "id": 925,
+      "city": "Barcelona",
+      "country": "Spain",
+      "tags": ["sport", "fashion", "events", "animals"],
+      "tagline": "Je crée des souvenirs",
+      "price": 275,
+      "portrait": "RhodeDubois.jpg"
+    },
+    {
+      "name": "Marcel Nikolic",
+      "id": 195,
+      "city": "Berlin",
+      "country": "Germany",
+      "tags": ["travel", "architecture"],
+      "tagline": "Toujours à la recherche de LA photo",
+      "price": 300,
+      "portrait": "MarcelNikolic.jpg"
+    }
+  ]
 
 
 
